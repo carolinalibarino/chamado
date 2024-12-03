156ea3f17e89004752368b51fb9cab39b525559a
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2024-12-03 15:17:18 -0300

    Add files via upload

--- suporte.sql
+++ suporte.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28-Nov-2024 às 16:36
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.0.25
+-- Tempo de geração: 03/12/2024 às 19:16
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,11 +26,11 @@ USE `suporte`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `chamado`
+-- Estrutura para tabela `chamado`
 --
 
 CREATE TABLE `chamado` (
-  `id` int(255) NOT NULL,
+  `id_chamado` int(255) NOT NULL,
   `tipo` varchar(255) NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `sala` varchar(255) NOT NULL,
@@ -44,11 +44,40 @@ CREATE TABLE `chamado` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `log_chamado`
+-- Estrutura para tabela `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `admistracao` varchar(20) NOT NULL,
+  `informatica` varchar(20) NOT NULL,
+  `quimica` varchar(20) NOT NULL,
+  `logista` varchar(20) NOT NULL,
+  `recursos_humanos` varchar(20) NOT NULL,
+  `contabilidade` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `informativos`
+--
+
+CREATE TABLE `informativos` (
+  `id_usuario` int(15) NOT NULL,
+  `data` datetime NOT NULL,
+  `informacao` mediumtext NOT NULL,
+  `orientacao` mediumtext NOT NULL,
+  `id_informativos` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `log_chamado`
 --
 
 CREATE TABLE `log_chamado` (
-  `id` int(255) NOT NULL,
+  `id_chamado` int(255) NOT NULL,
   `tipo` varchar(255) NOT NULL,
   `usuario` varchar(255) NOT NULL,
   `sala` varchar(255) NOT NULL,
@@ -64,22 +93,47 @@ CREATE TABLE `log_chamado` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `log_perfil`
+-- Estrutura para tabela `log_informativos`
 --
 
-CREATE TABLE `log_perfil` (
-  `suporte` varchar(255) DEFAULT NULL,
-  `padrao` varchar(255) DEFAULT NULL,
-  `secretaria` varchar(255) DEFAULT NULL,
-  `adm` varchar(255) DEFAULT NULL,
+CREATE TABLE `log_informativos` (
+  `id_usuario` int(11) NOT NULL,
+  `data` datetime NOT NULL,
+  `informacao` mediumtext NOT NULL,
+  `orientacao` mediumtext NOT NULL,
   `mudanca` varchar(255) NOT NULL,
-  `crud` varchar(10) NOT NULL
+  `CRUD` varchar(12) NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `id_informativos` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `log_usuario`
+-- Estrutura para tabela `log_reserva`
+--
+
+CREATE TABLE `log_reserva` (
+  `id_reserva` int(11) NOT NULL,
+  `nome_event` varchar(50) NOT NULL,
+  `curso` varchar(25) NOT NULL,
+  `turma` varchar(10) NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `tipo_usuario` int(11) NOT NULL,
+  `equipamentos` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fim` time NOT NULL,
+  `objetivo` mediumtext NOT NULL,
+  `mudanca` mediumtext NOT NULL,
+  `crud` varchar(12) NOT NULL,
+  `data_hora` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `log_usuario`
 --
 
 CREATE TABLE `log_usuario` (
@@ -87,38 +141,83 @@ CREATE TABLE `log_usuario` (
   `senha` varchar(255) DEFAULT NULL,
   `telefone` int(11) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  `id` int(255) NOT NULL,
+  `id_usuario` int(255) NOT NULL,
   `mudanca` varchar(255) NOT NULL,
   `crud` varchar(10) NOT NULL,
-  `data_hora` datetime NOT NULL
+  `data_hora` datetime NOT NULL,
+  `validacaoCadastro` varchar(255) NOT NULL,
+  `statusAtividade` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `perfil`
+-- Estrutura para tabela `reserva`
 --
 
-CREATE TABLE `perfil` (
-  `adm` varchar(255) DEFAULT NULL,
-  `suporte` varchar(255) DEFAULT NULL,
-  `padrao` varchar(255) DEFAULT NULL,
-  `secretaria` varchar(255) DEFAULT NULL
+CREATE TABLE `reserva` (
+  `id_reserva` int(11) NOT NULL,
+  `nome_event` varchar(50) NOT NULL,
+  `curso` varchar(25) NOT NULL,
+  `turmas` varchar(10) NOT NULL,
+  `local` varchar(255) NOT NULL,
+  `tipo_usuario` int(11) NOT NULL,
+  `equipamentos` varchar(255) NOT NULL,
+  `data` date NOT NULL,
+  `hora_inicio` time NOT NULL,
+  `hora_fim` time NOT NULL,
+  `objetivo` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `usuario`
+-- Estrutura para tabela `turmas`
+--
+
+CREATE TABLE `turmas` (
+  `1A` int(15) NOT NULL,
+  `2A` int(15) NOT NULL,
+  `3A` int(15) NOT NULL,
+  `1B` int(15) NOT NULL,
+  `2B` int(15) NOT NULL,
+  `3B` int(15) NOT NULL,
+  `1C` int(15) NOT NULL,
+  `2C` int(15) NOT NULL,
+  `3C` int(15) NOT NULL,
+  `1F` int(15) NOT NULL,
+  `2F` int(15) NOT NULL,
+  `3F` int(15) NOT NULL,
+  `1I` int(15) NOT NULL,
+  `2I` int(15) NOT NULL,
+  `3I` int(15) NOT NULL,
+  `1G` int(15) NOT NULL,
+  `2G` int(15) NOT NULL,
+  `1H` int(15) NOT NULL,
+  `2H` int(15) NOT NULL,
+  `3H` int(15) NOT NULL,
+  `1D` int(15) NOT NULL,
+  `2D` int(15) NOT NULL,
+  `3D` int(15) NOT NULL,
+  `1K` int(15) NOT NULL,
+  `2K` int(15) NOT NULL,
+  `3K` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
 --
 
 CREATE TABLE `usuario` (
-  `id` int(255) NOT NULL,
+  `id_usuario` int(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `email` varchar(128) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `telefone` int(11) DEFAULT NULL,
-  `perfil` int(255) DEFAULT NULL
+  `telefone` varchar(11) DEFAULT NULL,
+  `validacaoCadastro` varchar(255) NOT NULL,
+  `statusAtividade` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
@@ -126,29 +225,111 @@ CREATE TABLE `usuario` (
 --
 
 --
--- Índices para tabela `chamado`
+-- Índices de tabela `chamado`
 --
 ALTER TABLE `chamado`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_chamado`);
 
 --
--- Índices para tabela `log_chamado`
+-- Índices de tabela `informativos`
+--
+ALTER TABLE `informativos`
+  ADD PRIMARY KEY (`id_informativos`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `log_chamado`
 --
 ALTER TABLE `log_chamado`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_chamado`);
 
 --
--- Índices para tabela `log_usuario`
+-- Índices de tabela `log_informativos`
+--
+ALTER TABLE `log_informativos`
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_informativo` (`id_informativos`);
+
+--
+-- Índices de tabela `log_reserva`
+--
+ALTER TABLE `log_reserva`
+  ADD KEY `id_reserva` (`id_reserva`);
+
+--
+-- Índices de tabela `log_usuario`
 --
 ALTER TABLE `log_usuario`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_usuario`);
 
 --
--- Índices para tabela `usuario`
+-- Índices de tabela `reserva`
+--
+ALTER TABLE `reserva`
+  ADD PRIMARY KEY (`id_reserva`);
+
+--
+-- Índices de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `chamado`
+--
+ALTER TABLE `chamado`
+  MODIFY `id_chamado` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `informativos`
+--
+ALTER TABLE `informativos`
+  MODIFY `id_informativos` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `reserva`
+--
+ALTER TABLE `reserva`
+  MODIFY `id_reserva` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `informativos`
+--
+ALTER TABLE `informativos`
+  ADD CONSTRAINT `informativos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Restrições para tabelas `log_chamado`
+--
+ALTER TABLE `log_chamado`
+  ADD CONSTRAINT `log_chamado_ibfk_1` FOREIGN KEY (`id_chamado`) REFERENCES `chamado` (`id_chamado`);
+
+--
+-- Restrições para tabelas `log_informativos`
+--
+ALTER TABLE `log_informativos`
+  ADD CONSTRAINT `log_informativos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
+  ADD CONSTRAINT `log_informativos_ibfk_2` FOREIGN KEY (`id_informativos`) REFERENCES `informativos` (`id_informativos`);
+
+--
+-- Restrições para tabelas `log_reserva`
+--
+ALTER TABLE `log_reserva`
+  ADD CONSTRAINT `log_reserva_ibfk_1` FOREIGN KEY (`id_reserva`) REFERENCES `reserva` (`id_reserva`);
+
+--
+-- Restrições para tabelas `log_usuario`
+--
+ALTER TABLE `log_usuario`
+  ADD CONSTRAINT `log_usuario_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
